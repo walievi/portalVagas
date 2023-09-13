9f8ea44e9878b6975e6c8081515c785af118c632
EduardaDalmas <eduardadalmas@ienh.com.br>
2023-09-13 20:59:54 -0300

    melhorias na tela home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,22 @@
 @extends('general')
 
 @section('content')
-<div>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8 mt-5">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="background">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 mt-5">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    {{ __('You are logged in!') }}
+                        {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
