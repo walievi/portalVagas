2268e4a377959bfb8a2288b39fd804db20c4c6eb
Eduarda Brisch Dalmás <du.dalmas@hotmail.com>
2023-09-16 17:05:58 -0300

    ajuste na rota de criar usuário

--- routes/web.php
+++ routes/web.php
@@ -28,11 +28,9 @@ Route::get('/vagas', [Controllers\VagasController::class, 'index'])->name('vagas
 
 Route::get('/users', [Controllers\UsersController::class, 'index'])->name('users');
 
-Route::get('/user', [Controllers\UsersController::class, 'create'])->name('users.create');
+Route::get('/user', [Controllers\UsersController::class, 'create'])->name('user');
 
 Route::delete('/users/{id}', [Controllers\UsersController::class, 'destroy'])->name('users.destroy');
-
-
 
 Route::middleware('web')->resource('curriculo', Controllers\CurriculoController::class);
 
