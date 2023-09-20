7b8fe9ffaa9158c4900aa7add47dfc5c8ce82c64
Guilhermegfssilva <guilhermefsilva2019@hotmail.com>
2023-09-20 20:26:47 -0300

    add migrations formulario table

---
+++ database/migrations/2023_09_20_232519_create_formularios_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormulariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formularios', function (Blueprint $table) {
+            $table->id();
+            $table->text('cursos_complementares')->nullable();
+            $table->unsignedBigInteger('vaga_id')->nullable(); // Chave estrangeira
+
+            // Definir a chave estrangeira
+            $table->foreign('vaga_id')
+            ->references('id')
+            ->on('vagas')
+            ->onDelete('SET NULL'); // Define a ação para quando a vaga for excluída
+
+            $table->timestamps();
+
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formularios');
+    }
+}
