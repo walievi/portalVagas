aa5edc7f614aeeadd312422d782259f536a00f61
Guilhermegfssilva <guilhermefsilva2019@hotmail.com>
2023-09-20 14:25:10 -0300

    Criado Model e tabela para vagas

--- app/Http/Controllers/VagasController.php
+++ app/Http/Controllers/VagasController.php
@@ -4,10 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Vagas;
+
 
 class VagasController extends Controller
 {
     public function index() {
         return view('vagas.index');
+    }
+
+
+    public function create(Request $request) {
+        
+        view('vagas.index');
+
+        Vagas::create([
+            'titulo' => $request->input('titulo'),
+            'status' => $request->input('status'),
+        ]);
+
+
+        return redirect()->route('vagas')->with('success', 'Vaga adicionada com sucesso.');
+
+
+
+
     }
 }
---
+++ database/migrations/2023_09_20_171647_create_vagas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVagasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vagas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('titulo', 100);
+            $table->string('status', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vagas');
+    }
+}
