0d6f2344241f03da8fa7b77308909f1d7e6b66f8
Eduarda Brisch Dalmás <du.dalmas@hotmail.com>
2023-09-22 21:47:44 -0300

    ajustada listagem e mensagem ao logar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,21 +4,16 @@
 @section('content')
 <div class="background">
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8 mt-5">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="col-md-12">
+            @if(Auth::user()->role == 'user')
+            <h2 class="text-center mt-5"> Olá,  {{ Auth::user()->name }} </h2>
+            <p  class="text-center">Envie seu currículo online e venha trabalhar conosco!</p>
+            @endif
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                    {{ __('Você está logado!') }}
-                </div>
-            </div>
+            @if(Auth::user()->role == 'admin')
+            <h2 class="text-center mt-5">{{ __('Vagas disponíveis') }}</h2>
+            <p  class="text-center">Gerencie suas vagas em aberto!</p>
+            @endif
         </div>
         <div class="row justify-content-center mt-5">
             @foreach ($vagas as $vaga)
@@ -31,16 +26,14 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                                 <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
                                 </svg>
-                                <!-- <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
-                                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
-                                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                                </svg> -->
                             </div>
                             <div class="col-md-9">
                                 <p class="card-text">{{ $vaga->unidade }}</p>
                             </div>
                         </div>
+                        @if(Auth::user()->role == 'user')
                         <a href="{{ route('login') }}" class="btn btn-dark mt-3"> Candidate-se</a>
+                        @endif
                     </div>
                 </div>
             </div>
