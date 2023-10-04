d6a3cad5ee08e14c6314f9dc1cfef4fa0e22e2c2
Guilhermegfssilva <guilhermefsilva2019@hotmail.com>
2023-10-04 20:11:55 -0300

    Commit criação models e migrations dados pessoais

---
+++ app/Models/DadosPessoais.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DadosPessoais extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'data_nascimento',
+        'endereco_id',
+        'formacao_id',
+        'contato_id',
+        'user_id',
+        'objetivo_vaga',
+        'habilidades',
+    ];
+
+}
