15b3bd0eb0c0bb309d7bc529d25da7399f29b237
Guilhermegfssilva <guilhermefsilva2019@hotmail.com>
2023-11-11 16:15:17 -0300

    ajuste retorno tela home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/home') }}">
                     {{ 'Portal de Vagas' }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
